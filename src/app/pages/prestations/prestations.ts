f8e46f68ed357d9d723b431dc71983f132110313
Fane Yaya <y.fane75@hotmail.fr>
2026-06-19 21:25:18 +0200

    new page presentation

--- src/app/pages/prestations/prestations.ts
+++ src/app/pages/prestations/prestations.ts
@@ -1,14 +1,32 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { trigger, transition, style, animate } from '@angular/animations';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-prestations',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatIconModule],
   templateUrl: './prestations.html',
   styleUrl: './prestations.scss',
 })
 export class PrestationsComponent {
+
+  animations =  [
+
+    trigger('fade', [
+
+      transition(':enter', [
+
+        style({ opacity: 0, transform: 'translateY(10px)' }),
+
+        animate('300ms ease-out', style({ opacity: 1, transform: 'translateY(0)' }))
+
+      ])
+
+    ])
+
+  ]
 
   selected = 'all';
 
