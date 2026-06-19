c7de6b4da2f779f528d73bbabae92a62508f7aa2
Fane Yaya <y.fane75@hotmail.fr>
2026-06-19 18:33:02 +0200

    test-1

--- src/app/pages/prestations/prestations.ts
+++ src/app/pages/prestations/prestations.ts
@@ -1,10 +1,60 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-prestations',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './prestations.html',
   styleUrl: './prestations.scss',
 })
-export class PrestationsComponent {}
+export class PrestationsComponent {
+  
+
+
+  prestations = [
+    
+
+    {
+
+      title: 'Tresses africaines',
+
+      desc: 'Stylisation protectrice adaptée à tous types de cheveux.',
+
+      price: 'À partir de 40€'
+
+    },
+
+    {
+
+      title: 'Nattes collées',
+
+      desc: 'Style élégant et durable pour toutes occasions.',
+
+      price: 'À partir de 30€'
+
+    },
+
+    {
+
+      title: 'Twists',
+
+      desc: 'Look naturel et moderne, léger et confortable.',
+
+      price: 'À partir de 50€'
+
+    },
+
+    {
+
+      title: 'Coiffure événementielle',
+
+      desc: 'Mariage, fête, shooting photo…',
+
+      price: 'Sur devis'
+
+    }
+
+  ];
+
+}
